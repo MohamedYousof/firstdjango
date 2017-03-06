99e7ab7ae07bad2f6dcccb721cd2b597bcda9ba7
Mohamed <muhamed.yousof@gmail.com>
2017-03-06 20:22:53 +0200

    from pycharm

--- inventory/models.py
+++ inventory/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Item(models.Model):
+    """for testing """
     title = models.CharField(max_length=200)
     description = models.TextField()
     amount = models.IntegerField()
